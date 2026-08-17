29174d9442678a467785d60d6716cd3391d15b1f
KSashwathiSri <sashwathisri@gmail.com>
2026-08-17 22:26:34 +0530

    Deploy Django ticket system

--- new/settings.py
+++ new/settings.py
@@ -126,14 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STORAGES = {
-    'default': {
-        'BACKEND': 'django.core.files.storage.FileSystemStorage',
-    },
-    'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    },
-}
 LOGIN_URL='login'
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='login'
